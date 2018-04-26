177cf6f81b6fb36ca07ff49820eb71e4f22b1981
deathride <seanmarshall1999@live.co.uk>
2018-04-27 00:55:49 +0100

    Russian roulette for files
    Deletes a random file in a directory you choose, doesnt delete folders or check inside them

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RussianRoulette
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Copy and paste folder directory");
+            Console.WriteLine("Add another \\ after each in directory name");
+            var dir = new DirectoryInfo(Convert.ToString(Console.ReadLine()));
+
+            char restart = 'y';
+            while (restart == 'y')
+            {
+                foreach (var file in dir.EnumerateFiles("*"))
+                {
+                    Random rdm = new Random();
+                    int shoot = rdm.Next(1, 7);
+                    if (shoot == 1)
+                    {
+                        Console.WriteLine("Deleted " + file);
+                        file.Delete();
+                        Console.WriteLine("Again? Y/N");
+                        restart = Convert.ToChar(Console.ReadLine());
+                    }
+                    else
+                    {
+                        Console.WriteLine("You got lucky");
+                        Console.WriteLine("Again? Y/N");
+                        restart = Convert.ToChar(Console.ReadLine());
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
